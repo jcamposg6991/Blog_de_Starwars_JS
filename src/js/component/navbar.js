35d2eb8ef3b72b55c4b1b31721bd5e416d40f91d
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-12 02:37:28 +0000

    navbar frontend coding

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,20 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light mb-3 ps-5">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src="https://logos-world.net/wp-content/uploads/2020/11/Star-Wars-Logo.png" alt="" width="200" height="100" />
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="ml-auto pe-5">
+				<div class="btn-group" role="group">
+					<button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+						Favorites
+					</button>
+					<ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+						<li><a class="dropdown-item" href="#">Dropdown link</a></li>
+						<li><a class="dropdown-item" href="#">Dropdown link</a></li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
