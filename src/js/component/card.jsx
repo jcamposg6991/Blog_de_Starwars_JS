265b5f0ce249aacfcc494bfb6f289b81b2b3b0e4
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-12 03:45:40 +0000

    carousel with card frontend  coding

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,40 +1,38 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
+import { Carousel } from "react-bootstrap";
 
 export const Card = () => {
-
-    const { store, actions } = useContext(Context)
+    const { store, actions } = useContext(Context);
 
     useEffect(() => {
-        actions.loadSomePeople()
-    }, [])
+        actions.loadSomePeople();
+    }, []);
 
     return (
-        <>
-
-            {store.people.map((item) => {
-                return (
-                    <>
-                        <div className="card" style={{ width: "18rem" }}>
-                            <img src="..." className="card-img-top" alt="..." />
+        <Carousel>
+            {store.people.map((item) => (
+                <Carousel.Item key={item.id}>
+                    <div className="d-flex justify-content-center">
+                        <div className="card" style={{marginBottom: "20px" }}>
+                            <img
+                                src="https://picsum.photos/800/500"
+                                className="card-img-top"
+                                alt={item.name}
+                            />
                             <div className="card-body">
-                                <h5 key={item.id} className="card-title">{item.name}</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                                <h5 className="card-title">{item.name}</h5>
+                                <p className="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                                <a href="#" className="btn btn-primary">
+                                    Go somewhere
+                                </a>
                             </div>
                         </div>
-                    </>
-                )
-            })}
-
-            <div className="card" style={{ width: "18rem" }}>
-                <img src="..." className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </>
-    )
-}
+                    </div>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
+};
