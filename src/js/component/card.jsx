7375eaf3d894e042d059a5a6ef34c2822e21486f
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2024-11-13 16:30:08 +0000

    card frontend coding display people properties

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -14,20 +14,27 @@ export const Card = () => {
             {store.people.map((item) => (
                 <Carousel.Item key={item.id}>
                     <div className="d-flex justify-content-center">
-                        <div className="card" style={{marginBottom: "20px" }}>
+                        <div className="card" style={{ marginBottom: "20px" }}>
                             <img
                                 src="https://picsum.photos/800/500"
                                 className="card-img-top"
                                 alt={item.name}
                             />
                             <div className="card-body">
-                                <h5 className="card-title">{item.name}</h5>
-                                <p className="card-text">
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                <h2 className="card-title">{item.name}</h2>
+                                <p className="card-text fw-light fs-4">
+                                    Gender: {item.gender}<br />
+                                    Hair color: {item.hair_color}<br />
+                                    Eye color: {item.eye_color}<br />
                                 </p>
-                                <a href="#" className="btn btn-primary">
-                                    Go somewhere
-                                </a>
+                                <div className="row">
+                                    <div className="col-6 d-flex justify-content-start">
+                                        <button type="button" class="btn btn-outline-primary ">Learn More!</button>
+                                    </div>
+                                    <div className="col-6 d-flex justify-content-end">
+                                        <button type="button" class="btn btn-outline-warning ">Heart</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -13,19 +13,37 @@ const getState = ({ getStore, getActions, setStore }) => {
 					initial: "white"
 				}
 			],
-			people: []
+			people: [],
+			PeopleDetalis:[]
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
+			
 			loadSomePeople: () => {
 				fetch("https://www.swapi.tech/api/people")
 					.then(res => res.json())
-					.then(data => setStore({ people: data.results }))
-					.catch(err => console.error(err))
+					.then(data => {
+						const peoplePromises = data.results.map(person =>
+							fetch(`https://www.swapi.tech/api/people/${person.uid}`)
+								.then(res => res.json())
+								.then(detail => ({
+									id: person.uid,
+									name: person.name,
+									gender: detail.result.properties.gender,
+									hair_color: detail.result.properties.hair_color,
+									eye_color: detail.result.properties.eye_color,
+									description: detail.result.description
+								}))
+						);
+						return Promise.all(peoplePromises);
+					})
+					.then(peopleDetails => setStore({ people: peopleDetails }))
+					.catch(err => console.error(err));
 			},
+
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
